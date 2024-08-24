b40f22221890e22a54d158b7b4eb6390594c2977
hammahutz <62766637+hammahutz@users.noreply.github.com>
2024-08-24 13:29:55 +0200

    Added diagnostic system

--- src/systems/plugins/debug.rs
+++ src/systems/plugins/debug.rs
@@ -1,4 +1,14 @@
-use bevy::{app::AppExit, math::bounding::BoundingVolume, prelude::*};
+use bevy::{
+    app::AppExit,
+    diagnostic::{
+        DiagnosticsStore, EntityCountDiagnosticsPlugin, FrameTimeDiagnosticsPlugin,
+        LogDiagnosticsPlugin,
+    },
+    math::bounding::BoundingVolume,
+    prelude::*,
+    sprite::Anchor,
+    window::PrimaryWindow,
+};
 
 use crate::data::components::{
     Ball, CircleCollider, HealthComponent, PaddleComponent, RectangleCollider, VelocityComponent,
@@ -15,14 +25,19 @@ impl Plugin for DebugPlugin {
         if !IS_DEBUG {
             return;
         }
-        app.add_systems(PostStartup, spawn_health_text)
+        app.add_systems(PostStartup, (spawn_health_text, spawn_specs_text))
             .add_systems(
                 Update,
                 (draw_collider, draw_vector, update_health_display)
                     .in_set(GameLoop::UpdateEntities),
             )
-            .add_systems(Update, exit_game)
-            .init_gizmo_group::<DebugGizmos>();
+            .add_systems(Update, (exit_game, update_specs_text))
+            .init_gizmo_group::<DebugGizmos>()
+            .add_plugins((
+                LogDiagnosticsPlugin::default(),
+                FrameTimeDiagnosticsPlugin,
+                EntityCountDiagnosticsPlugin,
+            ));
     }
 }
 
@@ -100,5 +115,76 @@ fn update_health_display(mut query: Query<(&mut Text, &HealthComponent)>) {
         let style = text.sections[0].style.clone();
 
         text.sections = vec![TextSection { value, style }];
+    }
+}
+
+#[derive(Component)]
+pub struct SpecText;
+
+fn spawn_specs_text(mut commands: Commands, windows: Query<&Window, With<PrimaryWindow>>) {
+    let window = windows.single();
+    let x = -window.resolution.width() / 2.0 + 50.0;
+    let y = window.resolution.height() / 2.0 - 50.0;
+
+    commands.spawn((
+        Text2dBundle {
+            text: Text::from_section(
+                String::from("FPS:"),
+                TextStyle {
+                    font_size: 20.0,
+                    color: Color::ORANGE,
+                    ..default()
+                },
+            ),
+            transform: Transform::from_xyz(x, y, 0.0),
+            text_anchor: Anchor::TopLeft,
+            ..default()
+        },
+        SpecText,
+    ));
+}
+
+fn update_specs_text(
+    mut query: Query<&mut Text, With<SpecText>>,
+    diagnostic: Res<DiagnosticsStore>,
+) {
+    let mut text = query.single_mut();
+    let style = text.sections[0].style.clone();
+    let mut values: Vec<String> = Vec::new();
+
+    if let Some(fps_diagnostic) = diagnostic.get(&FrameTimeDiagnosticsPlugin::FPS) {
+        if let Some(fps_value) = fps_diagnostic.smoothed() {
+            values.push(format!("FPS: {:.0}", fps_value));
+        }
+    }
+
+    if let Some(frame_time_diagnostic) = diagnostic.get(&FrameTimeDiagnosticsPlugin::FRAME_TIME) {
+        if let Some(frame_time_value) = frame_time_diagnostic.smoothed() {
+            values.push(format!("Frame time: {:.2}", frame_time_value));
+        }
+    }
+
+    if let Some(frame_count_diagnostic) = diagnostic.get(&FrameTimeDiagnosticsPlugin::FRAME_COUNT) {
+        if let Some(frame_count_value) = frame_count_diagnostic.smoothed() {
+            values.push(format!("Frame count: {:.0}", frame_count_value));
+        }
+    }
+
+    if let Some(entity_count_diagnostic) =
+        diagnostic.get(&EntityCountDiagnosticsPlugin::ENTITY_COUNT)
+    {
+        if let Some(entity_count_value) = entity_count_diagnostic.smoothed() {
+            values.push(format!("Entitys count: {:.0}", entity_count_value));
+        }
+    }
+
+    if !values.is_empty() {
+        text.sections = values
+            .into_iter()
+            .map(|value| TextSection {
+                value: format!("{}\n", value),
+                style: style.clone(),
+            })
+            .collect();
     }
 }
